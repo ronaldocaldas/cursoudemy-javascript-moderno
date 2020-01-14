f924e4781dc0d16cc518c1ef1051441a378a83f2
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-14 16:55:16 -0200

    Aula 60: estrutura For

---
+++ controle/for1.js
@@ -0,0 +1,22 @@
+let contador = 1
+
+while(contador <=10){
+    console.log(`contatdor = ${contador}`);
+    contador++    
+}
+
+
+for (let index = 0; index < 10; index++) {
+    
+    console.log(` index = ${index}`);
+    
+}
+
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for (let index = 0; index < notas.length; index++) {
+    const element = notas[index];
+    console.log(`nota = ${element}` );
+    
+}
