376faa9dbab67c535cead0c8638d69e8268539d1
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-16 10:41:17 -0200

    Aula 81: factory #01

---
+++ funcao/factory1.js
@@ -0,0 +1,17 @@
+const prod1 = {
+    nome: 'Nome',
+    preco: 45
+}
+
+
+
+// factory simples
+
+function criarPessoa() {
+    return {
+        nome: 'ana',
+        sobrenome: 'silva'
+    }
+}
+
+console.log(criarPessoa());
