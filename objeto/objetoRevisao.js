f410d4afd9327836c29e3f27a251656833dd5a14
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-17 16:06:26 -0200

    Aula 88: revisão sobre Objeto

---
+++ objeto/objetoRevisao.js
@@ -0,0 +1,45 @@
+// coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Genérica'
+produto.preco = 220
+console.log(produto);
+
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto);
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua abc',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nume: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 19
+    }],
+    calcularValorSeguro: function(){
+        //...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av gigante'
+console.log(carro);
+
+//delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+
+console.log(carro);
+console.log(carro.condutores);
+console.log(carro.condutores.length);
