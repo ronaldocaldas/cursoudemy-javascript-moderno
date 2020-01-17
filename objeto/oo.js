0be8ad675c9742ec422d856a3f5e7929b03936f4
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-17 15:42:09 -0200

    Aula 87: introdução à OO

---
+++ objeto/oo.js
@@ -0,0 +1,25 @@
+// Código não executavel  !!
+
+// Procedural 
+processamento(valor1, valor2, valor3)
+
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+// Principios importantes
+//  1. Abstração
+//  2. encapsulamento
+//  3. herança (prototype)
+//  4. polimorfismo
+
+
+
