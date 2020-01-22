1882e7eba19b33ca67524a44459c1d27acb0e994
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-22 09:58:23 -0200

    Aula 112: filter #01

---
+++ array/filter1.js
@@ -0,0 +1,12 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+const fragil = f => f.fragil
+const caro = c => c.preco > 500
+console.log(produtos.filter(fragil).filter(caro))
+
+
