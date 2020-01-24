ad7a49f3611c9fec9af9771721596ef2b6ceb2b3
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-24 10:11:10 -0200

    Aula 123: sistema de módules require

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola);
+
+const http = require('http')
+
+const c = require('./pastaC/index')
+console.log(c.ola2);
+
+// http.createServer((req, res) =>{
+//     res.write('Bom dia')
+//     res.end()
+// }).listen(8080)
+
