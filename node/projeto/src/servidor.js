41999f2a1f7155bd10ac29426073f8d700f5399a
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-28 21:55:04 -0200

    Aula 138: API com express #01

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,20 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) =>{
+   console.log('Middleware 1');
+   next()
+   
+})
+
+
+app.get('/produtos', (req, res, next) =>{
+    res.send({nome: 'Notebook', preco: 123.45}) // converter para JSON
+
+})
+app.listen(porta, () =>{
+    console.log(`Servidor está executando na porta ${porta}.`);
+    
+})
