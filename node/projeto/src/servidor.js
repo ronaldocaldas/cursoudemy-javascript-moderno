cb8229f9087b57304f630504f5291d2d397c2980
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-28 22:10:16 -0200

    Aula 139: projeto: API com express #02

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,19 +2,27 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, next) =>{
-   console.log('Middleware 1');
-   next()
-   
-})
-
-
-app.get('/produtos', (req, res, next) =>{
-    res.send({nome: 'Notebook', preco: 123.45}) // converter para JSON
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos) // converter para JSON
 
 })
-app.listen(porta, () =>{
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('./produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`);
-    
+
 })
+
