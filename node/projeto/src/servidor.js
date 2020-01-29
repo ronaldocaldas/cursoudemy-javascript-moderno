8300f797339a8c9ff50ece36332578388955ceb0
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2020-01-28 22:34:54 -0200

    Aula 140: projeto: API com Express #03

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,10 +2,13 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/produtos', (req, res, next) => {
-    res.send(bancoDeDados.getProdutos) // converter para JSON
+    res.send(bancoDeDados.getProdutos()) // converter para JSON
 
 })
 
@@ -13,13 +16,28 @@ app.get('/produtos/:id', (req, res, next) => {
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('./produtos', (req, res, next) => {
+app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto)
 })
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`);
